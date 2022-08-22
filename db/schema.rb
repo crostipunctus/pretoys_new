3bbda1aa7d1627cfd15281c6247f067570bf487a
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2022-08-22 16:26:18 +0530

    added user ref to toys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_105338) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_105538) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,7 +54,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_105338) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
+    t.integer "user_id", null: false
     t.index ["category_id"], name: "index_toys_on_category_id"
+    t.index ["user_id"], name: "index_toys_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -76,4 +78,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_105338) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "toys", "categories"
+  add_foreign_key "toys", "users"
 end
